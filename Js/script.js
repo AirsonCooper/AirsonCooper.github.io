8fac88e060f44f36b8b5690a6369cda7284b3fe3
Airson <ghost851014@gmail.com>
2019-11-08 20:25:05 +0800

    update

--- Js/script.js
+++
@@ -1,3 +0,0 @@
-document.write('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script>');
-document.write('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>');
-document.write('<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>');
